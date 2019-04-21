b47bd1a937d95452b2b2244262af5d8f4bb91571
Martha Czerwik <47041959+marthaczerwik@users.noreply.github.com>
2019-04-20 22:24:06 -0400

    Update BlackjackPlayerTest.java

--- PlayBlackjack/test/playblackjack/BlackjackPlayerTest.java
+++ PlayBlackjack/test/playblackjack/BlackjackPlayerTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package playblackjack;
 
 import org.junit.After;
@@ -16,41 +11,29 @@ import static org.junit.Assert.*;
  *
  * @author shann
  */
-public class BlackjackPlayerTest {
-   
-   public BlackjackPlayerTest() {
+public class PlayBlackjackTest {
+
+   public PlayBlackjackTest() {
    }
-   
+
    @BeforeClass
    public static void setUpClass() {
    }
-   
+
    @AfterClass
    public static void tearDownClass() {
    }
-   
+
    @Before
    public void setUp() {
    }
-   
+
    @After
    public void tearDown() {
    }
 
    /**
-    * Test of play method, of class BlackjackPlayer.
-//    */
-//   @Test
-//   public void testPlay() {
-//      System.out.println("play");
-//      BlackjackPlayer instance = null;
-//      instance.play();
-//      // TODO review the generated test code and remove the default call to fail.
-//      fail("The test case is a prototype.");
-//   }
-
-   /**
-    * Test of betMoney method, of class BlackjackPlayer.
+    * Test of good bet amount (less than the balance).
     */
    @Test
    public void testBetMoneyGood() {
@@ -58,81 +41,241 @@ public class BlackjackPlayerTest {
       double bet = 1;
       boolean expectedResult = true;
       Player p2 = new BlackjackPlayer("player", 1000);
-      boolean result=((BlackjackPlayer)(p2)).getMoney(bet) < (p2.getBalance());
+      boolean result = ((BlackjackPlayer) (p2)).getMoney(bet) < (p2.getBalance());
       assertEquals(expectedResult, result);
    }
-}
 
    /**
-    * Test of getName method, of class BlackjackPlayer.
+    * Test of bad bet amount (greater than the balance).
     */
-//   @Test
-//   public void testGetName() {
-//      System.out.println("getName");
-//      BlackjackPlayer instance = null;
-//      String expResult = "";
-//      String result = instance.getName();
-//      assertEquals(expResult, result);
-//      // TODO review the generated test code and remove the default call to fail.
-//      fail("The test case is a prototype.");
-//   }
-//
-//   /**
-//    * Test of setHand method, of class BlackjackPlayer.
-//    */
-//   @Test
-//   public void testSetHand() {
-//      System.out.println("setHand");
-//      int[][] cards = null;
-//      BlackjackPlayer instance = null;
-//      instance.setHand(cards);
-//      // TODO review the generated test code and remove the default call to fail.
-//      fail("The test case is a prototype.");
-//   }
-//
-//   /**
-//    * Test of addToBalanceWin method, of class BlackjackPlayer.
-//    */
-//   @Test
-//   public void testAddToBalanceWin() {
-//      System.out.println("addToBalanceWin");
-//      double bet = 0.0;
-//      BlackjackPlayer instance = null;
-//      double expResult = 0.0;
-//      double result = instance.addToBalanceWin(bet);
-//      assertEquals(expResult, result, 0.0);
-//      // TODO review the generated test code and remove the default call to fail.
-//      fail("The test case is a prototype.");
-//   }
-//
-//   /**
-//    * Test of addToBalancePlayerGetsBlackJack method, of class BlackjackPlayer.
-//    */
-//   @Test
-//   public void testAddToBalancePlayerGetsBlackJack() {
-//      System.out.println("addToBalancePlayerGetsBlackJack");
-//      double bet = 0.0;
-//      BlackjackPlayer instance = null;
-//      double expResult = 0.0;
-//      double result = instance.addToBalancePlayerGetsBlackJack(bet);
-//      assertEquals(expResult, result, 0.0);
-//      // TODO review the generated test code and remove the default call to fail.
-//      fail("The test case is a prototype.");
-//   }
-//
-//   /**
-//    * Test of gameDraw method, of class BlackjackPlayer.
-//    */
-//   @Test
-//   public void testGameDraw() {
-//      System.out.println("gameDraw");
-//      double bet = 0.0;
-//      BlackjackPlayer instance = null;
-//      double expResult = 0.0;
-//      double result = instance.gameDraw(bet);
-//      assertEquals(expResult, result, 0.0);
-//      // TODO review the generated test code and remove the default call to fail.
-//      fail("The test case is a prototype.");
-//   }
-//   
-//}
+   @Test
+   public void testBetMoneyBad() {
+      System.out.println("Bad Bet");
+      double bet = 100000;
+      boolean expectedResult = false;
+      Player p2 = new BlackjackPlayer("player", 1000);
+      boolean result = ((BlackjackPlayer) (p2)).getMoney(bet) > (p2.getBalance());
+      assertEquals(expectedResult, result);
+   }
+
+   /**
+    * Test of bet at the boundary.
+    */
+   @Test
+   public void testBetBoundry() {
+      System.out.println("Bet Boundary");
+      double bet = 1000;
+      boolean expectedResult = false;
+      Player p2 = new BlackjackPlayer("player", 1000);
+      boolean result = ((BlackjackPlayer) (p2)).getMoney(bet) == (p2.getBalance());
+      assertEquals(expectedResult, result);
+   }
+
+       /**
+     * Test of declareWinner method for draw good, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerDrawGood() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 21;
+        int sumPlayer = 21;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge == 0) {
+            System.out.println("Draw");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for draw bad, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerDrawBad() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 25;
+        int sumPlayer = 20;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge == 0) {
+            System.out.println("Not a draw");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for draw boundary, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerDrawBoundary() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 22;
+        int sumPlayer = 22;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge == 0) {
+            System.out.println("Draw");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for player good, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerPlayerGood() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 17;
+        int sumPlayer = 21;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge == 0) {
+            System.out.println("Player wins");
+        }
+    }
+        @Test
+    public void testDeclareWinnerPlayerGoodDealerBust() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 25;
+        int sumPlayer = 21;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge == 0) {
+            System.out.println("Player wins");
+        }
+    }
+  @Test
+    public void testDeclareWinnerPlayerGoodPlayerBust() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 20;
+        int sumPlayer = 25;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge == 0) {
+            System.out.println("Dealer wins");
+        }
+    }
+    /**
+     * Test of declareWinner method for player boundary, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerPlayerBoundary() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 20;
+        int sumPlayer = 21;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge < 0) {
+            System.out.println("Player wins");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for player bad, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerPlayerBad() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 20;
+        int sumPlayer = 19;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge < 0) {
+            System.out.println("Player loses");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for dealer good, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerDealerGood() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 20;
+        int sumPlayer = 18;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge > 0) {
+            System.out.println("Dealer wins");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for dealer boundary, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerDealerBoundary() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 20;
+        int sumPlayer = 19;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge > 0) {
+            System.out.println("Dealer wins");
+        }
+
+    }
+
+    /**
+     * Test of declareWinner method for dealer bad, of class PlayBlackjack.
+     */
+    @Test
+    public void testDeclareWinnerDealerBad() {
+        System.out.println("declareWinner");
+        int situation = 0;
+        PlayBlackjack instance = new PlayBlackjack();
+        instance.declareWinner(situation);
+        int sumDealer = 19;
+        int sumPlayer = 20;
+
+        int judge = sumPlayer - sumDealer;
+
+        if (judge > 0) {
+            System.out.println("Dealer loses");
+        }
+
+    }
+}
